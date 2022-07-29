792063b80537b9e05ef565b9c35027430186e3d3
Evelyn Cristini Oliveira <evelyn.oliveira@mercadolivre.com>
2022-07-28 22:11:32 -0300

    Add P II solução exercício I parte B de POO II

--- src/PraticaExcecoes.java
+++ src/PraticaExcecoes.java
@@ -1,16 +1,18 @@
 public class PraticaExcecoes {
 
+    private static int a = 0, b = 300;
     public static void main(String[] args) {
 
         dividir();
     }
 
     public static void dividir() {
-        int a = 0, b = 300;
 
-        try { double resultado = b / a; }
-        catch (ArithmeticException exception) { System.out.println("Ocorreu um erro: " + exception.getMessage()); }
-        finally { System.out.println("Programa finalizado."); }
-
+        try {
+            if (a == 0)
+                throw new IllegalArgumentException("Não pode ser dividido por zero.");
+        } catch (IllegalArgumentException exception) {
+        exception.printStackTrace();
+        }
     }
 }
